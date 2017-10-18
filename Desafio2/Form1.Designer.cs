cd79874e20cf48a74729c3c2f7585cf09dc44f79
alexprado <alexsilvadoprado@gmail.com>
2017-10-18 09:55:44 -0300

    Versão 1.0 (Estável)

--- Desafio2/Form1.Designer.cs
+++ Desafio2/Form1.Designer.cs
@@ -29,12 +29,12 @@
         private void InitializeComponent()
         {
             this.gpbLimites = new System.Windows.Forms.GroupBox();
-            this.lblTipos = new System.Windows.Forms.Label();
-            this.lblMinValue = new System.Windows.Forms.Label();
-            this.lblMaxValue = new System.Windows.Forms.Label();
-            this.cbTipos = new System.Windows.Forms.ComboBox();
-            this.txbMinValue = new System.Windows.Forms.TextBox();
             this.txbMaxValue = new System.Windows.Forms.TextBox();
+            this.txbMinValue = new System.Windows.Forms.TextBox();
+            this.cbTipos = new System.Windows.Forms.ComboBox();
+            this.lblMaxValue = new System.Windows.Forms.Label();
+            this.lblMinValue = new System.Windows.Forms.Label();
+            this.lblTipos = new System.Windows.Forms.Label();
             this.gpbLimites.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -51,62 +51,10 @@
             this.gpbLimites.Controls.Add(this.lblTipos);
             this.gpbLimites.Location = new System.Drawing.Point(12, 12);
             this.gpbLimites.Name = "gpbLimites";
-            this.gpbLimites.Size = new System.Drawing.Size(306, 122);
+            this.gpbLimites.Size = new System.Drawing.Size(360, 122);
             this.gpbLimites.TabIndex = 0;
             this.gpbLimites.TabStop = false;
             this.gpbLimites.Text = "Limites das Variáveis";
-            // 
-            // lblTipos
-            // 
-            this.lblTipos.AutoSize = true;
-            this.lblTipos.Location = new System.Drawing.Point(6, 36);
-            this.lblTipos.Name = "lblTipos";
-            this.lblTipos.Size = new System.Drawing.Size(84, 13);
-            this.lblTipos.TabIndex = 0;
-            this.lblTipos.Text = "Tipo de Variável";
-            // 
-            // lblMinValue
-            // 
-            this.lblMinValue.AutoSize = true;
-            this.lblMinValue.Location = new System.Drawing.Point(36, 63);
-            this.lblMinValue.Name = "lblMinValue";
-            this.lblMinValue.Size = new System.Drawing.Size(54, 13);
-            this.lblMinValue.TabIndex = 1;
-            this.lblMinValue.Text = "Min Value";
-            // 
-            // lblMaxValue
-            // 
-            this.lblMaxValue.AutoSize = true;
-            this.lblMaxValue.Location = new System.Drawing.Point(33, 89);
-            this.lblMaxValue.Name = "lblMaxValue";
-            this.lblMaxValue.Size = new System.Drawing.Size(57, 13);
-            this.lblMaxValue.TabIndex = 2;
-            this.lblMaxValue.Text = "Max Value";
-            // 
-            // cbTipos
-            // 
-            this.cbTipos.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.cbTipos.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cbTipos.FormattingEnabled = true;
-            this.cbTipos.Location = new System.Drawing.Point(96, 33);
-            this.cbTipos.Name = "cbTipos";
-            this.cbTipos.Size = new System.Drawing.Size(197, 21);
-            this.cbTipos.TabIndex = 3;
-            this.cbTipos.SelectedIndexChanged += new System.EventHandler(this.cbTipos_SelectedIndexChanged);
-            // 
-            // txbMinValue
-            // 
-            this.txbMinValue.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.txbMinValue.BackColor = System.Drawing.SystemColors.Control;
-            this.txbMinValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txbMinValue.ForeColor = System.Drawing.Color.Green;
-            this.txbMinValue.Location = new System.Drawing.Point(96, 60);
-            this.txbMinValue.Name = "txbMinValue";
-            this.txbMinValue.ReadOnly = true;
-            this.txbMinValue.Size = new System.Drawing.Size(197, 20);
-            this.txbMinValue.TabIndex = 4;
             // 
             // txbMaxValue
             // 
@@ -118,14 +66,66 @@
             this.txbMaxValue.Location = new System.Drawing.Point(96, 86);
             this.txbMaxValue.Name = "txbMaxValue";
             this.txbMaxValue.ReadOnly = true;
-            this.txbMaxValue.Size = new System.Drawing.Size(197, 20);
+            this.txbMaxValue.Size = new System.Drawing.Size(251, 20);
             this.txbMaxValue.TabIndex = 5;
+            // 
+            // txbMinValue
+            // 
+            this.txbMinValue.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txbMinValue.BackColor = System.Drawing.SystemColors.Control;
+            this.txbMinValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbMinValue.ForeColor = System.Drawing.Color.Green;
+            this.txbMinValue.Location = new System.Drawing.Point(96, 60);
+            this.txbMinValue.Name = "txbMinValue";
+            this.txbMinValue.ReadOnly = true;
+            this.txbMinValue.Size = new System.Drawing.Size(251, 20);
+            this.txbMinValue.TabIndex = 4;
+            // 
+            // cbTipos
+            // 
+            this.cbTipos.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbTipos.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbTipos.FormattingEnabled = true;
+            this.cbTipos.Location = new System.Drawing.Point(96, 33);
+            this.cbTipos.Name = "cbTipos";
+            this.cbTipos.Size = new System.Drawing.Size(251, 21);
+            this.cbTipos.TabIndex = 3;
+            this.cbTipos.SelectedIndexChanged += new System.EventHandler(this.cbTipos_SelectedIndexChanged);
+            // 
+            // lblMaxValue
+            // 
+            this.lblMaxValue.AutoSize = true;
+            this.lblMaxValue.Location = new System.Drawing.Point(33, 89);
+            this.lblMaxValue.Name = "lblMaxValue";
+            this.lblMaxValue.Size = new System.Drawing.Size(57, 13);
+            this.lblMaxValue.TabIndex = 2;
+            this.lblMaxValue.Text = "Max Value";
+            // 
+            // lblMinValue
+            // 
+            this.lblMinValue.AutoSize = true;
+            this.lblMinValue.Location = new System.Drawing.Point(36, 63);
+            this.lblMinValue.Name = "lblMinValue";
+            this.lblMinValue.Size = new System.Drawing.Size(54, 13);
+            this.lblMinValue.TabIndex = 1;
+            this.lblMinValue.Text = "Min Value";
+            // 
+            // lblTipos
+            // 
+            this.lblTipos.AutoSize = true;
+            this.lblTipos.Location = new System.Drawing.Point(6, 36);
+            this.lblTipos.Name = "lblTipos";
+            this.lblTipos.Size = new System.Drawing.Size(84, 13);
+            this.lblTipos.TabIndex = 0;
+            this.lblTipos.Text = "Tipo de Variável";
             // 
             // Desafio2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(330, 146);
+            this.ClientSize = new System.Drawing.Size(384, 146);
             this.Controls.Add(this.gpbLimites);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Desafio2";
--- Desafio2/Form1.cs
+++ Desafio2/Form1.cs
@@ -14,30 +14,49 @@ namespace Desafio2
     {
         public Desafio2()
         {
+            /** INICIALIZA A JANELA E OS COMPONENTES DA MESMA */
             InitializeComponent();
 
             InicializaComboBox();
         }
 
+        /** INICIALIZAÇÃO DO COMBOBOX */
         private void InicializaComboBox()
         {
-            //Criação dos itens do ComboBox
-            Item item1 = new Item("byte", byte.MaxValue, byte.MinValue);
-            Item item2 = new Item("sbyte", sbyte.MaxValue, sbyte.MinValue);
-            Item item3 = new Item("short", short.MaxValue, short.MinValue);
-            Item item4 = new Item("int", int.MaxValue, int.MinValue);
+            /** CRIAÇÃO DOS ITENS DO COMBOBOX */
+            Item itemByte = new Item("byte", byte.MaxValue, byte.MinValue);
+            Item itemSByte = new Item("sbyte", sbyte.MaxValue, sbyte.MinValue);
+            Item itemShort = new Item("short", short.MaxValue, short.MinValue);
+            Item itemUShort = new Item("ushort", ushort.MaxValue, ushort.MinValue);
+            Item itemInt = new Item("int", int.MaxValue, int.MinValue);
+            Item itemUInt = new Item("uint", uint.MaxValue, uint.MinValue);
+            Item itemLong = new Item("long", long.MaxValue, long.MinValue);
+            Item itemULong = new Item("ulong", ulong.MaxValue, ulong.MinValue);
+            Item itemFloat = new Item("float", float.MaxValue, float.MinValue);
+            Item itemDouble = new Item("double", double.MaxValue, double.MinValue);
+            Item itemDecimal = new Item("decimal", decimal.MaxValue, decimal.MinValue);
 
-            //Adiciona os itens ao ComboBox
-            cbTipos.Items.Add(item1);
-            cbTipos.Items.Add(item2);
-            cbTipos.Items.Add(item3);
-            cbTipos.Items.Add(item4);
+            /** ADICIONA OS ITENS AO COMBOBOX */
+            cbTipos.Items.Add(itemByte);
+            cbTipos.Items.Add(itemSByte);
+            cbTipos.Items.Add(itemShort);
+            cbTipos.Items.Add(itemUShort);
+            cbTipos.Items.Add(itemInt);
+            cbTipos.Items.Add(itemUInt);
+            cbTipos.Items.Add(itemLong);
+            cbTipos.Items.Add(itemULong);
+            cbTipos.Items.Add(itemFloat);
+            cbTipos.Items.Add(itemDouble);
+            cbTipos.Items.Add(itemDecimal);
         }
 
+        /** EVENTO DE SELEÇÃO DO COMBOBOX */
         private void cbTipos_SelectedIndexChanged(object sender, EventArgs e)
         {
+            /** PEGA REFERÊNCIA DO ITEM SELECIONADO */
             Item itemSelecionado = (Item)cbTipos.SelectedItem;
 
+            /** ADICIONA VALORES MÁXIMO E MÍNIMO AOS CAMPOS */
             txbMinValue.Text = itemSelecionado.valMin.ToString();
             txbMaxValue.Text = itemSelecionado.valMax.ToString();
         }
